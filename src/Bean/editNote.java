f8045d2e2fbfdfe74895183612a6f982a32162dc
ayanokouji-road <32695070+ayanokouji-road@users.noreply.github.com>
2017-11-13 01:53:53 +0800

    Create editNote.java

---
+++ src/Bean/editNote.java
@@ -0,0 +1,26 @@
+package Bean;
+
+import java.util.Map;
+
+import com.opensymphony.xwork2.ActionContext;
+
+public class editNote {
+	public boolean loggedin;
+	public boolean getLoggedin() {
+		return loggedin;
+	}
+	public void setLoggedin(boolean loggedin) {
+		this.loggedin=loggedin;
+	}
+	public editNote() {
+		Map<String, Object> session1=ActionContext.getContext().getSession();
+		if (!session1.containsKey("username")) {
+			loggedin=false;
+			return;
+		}
+		loggedin=true;
+		String username=(String)session1.get("username");
+		
+		
+	}
+}
