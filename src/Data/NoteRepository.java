17077dc5fa1ee17751c0352c72df2d60291466ce
Seeker <tzzxy7@foxmail.com>
2017-11-13 02:01:41 +0800

    Note类的数据库操作
    包括新建一个Note(旅游攻略)，展示所有Note，展示单个Note详情

---
+++ src/Data/NoteRepository.java
@@ -0,0 +1,81 @@
+package Data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import com.opensymphony.xwork2.ActionContext;
+
+import Class.Note;
+
+public class NoteRepository {
+	Connection con = null;
+    Statement stat = null;
+    ResultSet rs = null;
+    
+    public NoteRepository() throws ClassNotFoundException, SQLException {
+        String driver = "com.mysql.jdbc.Driver";
+        String username = "root";
+        String password = "123456";
+        String dbUrl = String.format("jdbc:mysql://%s:%s/%s?useUnicode=true&characterEncoding=utf8",
+                new Object[] { "localhost", "3306", "tourba" });
+
+        Class.forName(driver);
+        this.con = DriverManager.getConnection(dbUrl, username, password);
+        this.stat = this.con.createStatement();
+    }
+    
+    public void close() throws SQLException {
+        if (this.con != null) {
+            this.con.close();
+        }
+    }
+    
+    public boolean addNote(Note note){
+    	String noteID=note.getNoteID();
+    	String noteTitle=note.getNoteTitle();
+    	String travelNote=note.getTravelNote();
+    	String ownerName=note.getOwnerName();
+    	java.sql.Time noteTime=note.getNoteTime();
+    	String sql="insert into note(noteID,noteTitle,travelNote,ownerName,noteTime)values('"+noteID+"','"+noteTitle+"','"+travelNote+"','"+ownerName+"','"+noteTime+"')";
+		try {
+			stat.execute(sql);
+			return true;
+		} catch (SQLException e) {
+			return false;
+		}
+	}
+    public boolean showAll() throws SQLException {
+    	String sql="select * from note";
+    	rs=stat.executeQuery(sql);
+    	ArrayList<Note> noteList=new ArrayList<Note>();
+    	while (rs.next()) {
+			Note n = new Note();
+			n.setNoteID(rs.getString(1));
+			n.setNoteTitle(rs.getString(2));
+			n.setTravelNote(rs.getString(3));
+			n.setOwnerName(rs.getString(4));
+			n.setNoteTime(rs.getTime(5));
+			noteList.add(n);
+		}
+    	ActionContext.getContext().put("noteList", noteList);
+    	return true;
+    }
+    public boolean oneNoteInfo(String noteID) throws SQLException {
+		String sql="select * from note where noteID='"+noteID+"'";
+		rs=stat.executeQuery(sql);
+		if (rs.next()) {
+			Note note=new Note();
+			note.setNoteID(rs.getString(1));
+			note.setNoteTitle(rs.getString(2));
+			note.setTravelNote(rs.getString(3));
+			note.setOwnerName(rs.getString(4));
+			note.setNoteTime(rs.getTime(5));
+			ActionContext.getContext().put("aNote", note);
+		}
+		return true;
+	}
+}
