429f1e9e4f42270333b8b4e6ef27ce77d8abf337
ayanokouji-road <32695070+ayanokouji-road@users.noreply.github.com>
2017-11-13 01:52:23 +0800

    Create MyGroups.java

---
+++ src/Bean/MyGroups.java
@@ -0,0 +1,85 @@
+package Bean;
+
+import java.util.ArrayList;
+import java.util.Map;
+import com.opensymphony.xwork2.ActionContext;
+import Data.GroupRepository;
+import Data.GroupUserRepository;
+
+public class MyGroups {
+    public ArrayList<GroupM> allmygroups;
+    public boolean loggedin;
+    public ArrayList<GroupM> getAllmygroups() {
+        return allmygroups;
+    }
+
+    public void setAllmygroups(ArrayList<GroupM> allmygroups) {
+        this.allmygroups = allmygroups;
+    }
+
+    public boolean getLoggedin() {
+        return loggedin;
+    }
+
+    public void setLoggedin(boolean loggedin) {
+        this.loggedin = loggedin;
+    }
+
+    public MyGroups() throws Exception {
+        Map<String, Object> session1 = ActionContext.getContext().getSession();
+        if (!session1.containsKey("username")) {
+            loggedin = false;
+            return;
+        }
+        loggedin = true;
+        String username = (String)session1.get("username");
+        
+        // 获取当前用户所属的所有小组ID
+        GroupUserRepository groupUserDao = new GroupUserRepository();
+        ArrayList<String> AllMyGroupsid = groupUserDao.mygroupid(username);
+        groupUserDao.close();
+
+        // 标记该用户在每个小组中是否管理员
+        allmygroups = new ArrayList<GroupM>();
+        GroupRepository grouprepository = new GroupRepository();
+        for (String s : AllMyGroupsid) {
+            String groupname = grouprepository.getgroup(s).getGroupname();
+            if (grouprepository.IsManager(username, s)) {
+                allmygroups.add(new GroupM(groupname, s, true));
+            } else {
+                allmygroups.add(new GroupM(groupname, s, false));
+            }
+        }
+        grouprepository.close();
+    }
+}
+class GroupM {
+    private String groupid;
+    private String groupname;
+    private boolean isManaged;
+    public boolean getIsManaged() {
+        return isManaged;
+    }
+    public String getGroupid() {
+        return groupid;
+    }
+    public String getGroupname() {
+        return groupname;
+    }
+    public void setGroupname(String groupname) {
+        this.groupname = groupname;
+    }
+    public void setGroupid(String groupid) {
+        this.groupid = groupid;
+    }
+    public void setManaged(boolean isManaged) {
+        this.isManaged = isManaged;
+    }
+    public GroupM() {
+    }
+    public GroupM(String groupname, String groupid, boolean isManaged) {
+        this.groupname = groupname;
+        this.groupid = groupid;
+        this.isManaged = isManaged;
+    }
+}
