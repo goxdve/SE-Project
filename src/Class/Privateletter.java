d8a27eb4f5ba3d6241f807fd1ef254ee3c29b6bb
ayanokouji-road <32695070+ayanokouji-road@users.noreply.github.com>
2017-12-05 17:02:12 +0800

    Create Privateletter.java

---
+++ src/Class/Privateletter.java
@@ -0,0 +1,63 @@
+package Class;
+
+public class Privateletter 
+{
+	public String privateletterID;
+	public String username;
+	public String receiver;
+	public String sender;
+	public String privateletterTitle;
+	public String privateletterTime;
+	public String content;
+	public long timestamp;
+	
+	public String getPrivateletterID() {
+		return privateletterID;
+	}
+	public void setPrivateletterID(String privateletterID) {
+		this.privateletterID = privateletterID;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getReceiver() {
+		return receiver;
+	}
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	public String getPrivateletterTitle() {
+		return privateletterTitle;
+	}
+	public void setPrivateletterTitle(String privateletterTitle) {
+		this.privateletterTitle = privateletterTitle;
+	}
+	public String getPrivateletterTime() {
+		return privateletterTime;
+	}
+	public void setPrivateletterTime(String privateletterTime) {
+		this.privateletterTime = privateletterTime;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
