330fe75053868f03c42d1f5bd972311fbde681ba
ayanokouji-road <32695070+ayanokouji-road@users.noreply.github.com>
2017-12-08 00:27:33 +0800

    Create OtherSpace.java

---
+++ src/Action/OtherSpace.java
@@ -0,0 +1,19 @@
+package Action;
+
+public class OtherSpace 
+{
+	public String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String execute()
+	{
+		
+		return "success";
+	}
+}
